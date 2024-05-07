88d445fedc8f8ed74dfc20d5fb4ff4c41e87a35f
dione24 <dioneniangaly@gmail.com>
2024-05-07 16:34:33 +0000

    Add password hashing to user registration form

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -6,10 +6,11 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
@@ -23,13 +24,19 @@ class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
             $entityManager->persist($user);
             $entityManager->flush();
             $this->addFlash('success', 'Utilisateur ajouté avec succès');
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,10 +3,14 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
@@ -18,21 +22,38 @@ class UserType extends AbstractType
                 'choices' => [
                     'Admin' => 'ROLE_ADMIN',
                     'User' => 'ROLE_USER',
+                    'Manager' => 'ROLE_MANAGER',
                     // Add more roles as needed
                 ],
                 'multiple' => true,
                 'expanded' => true, // Display checkboxes for multiple selection
             ])
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('email');
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add(
+                'email',
+                EmailType::class
+            );
+        // je conditionne mon formulaire selon si on edit ou pas
+        if ($options["custom_option"] !== "edit") {
+            $builder->add('password', RepeatedType::class, [
+                "type" => PasswordType::class,
+                "first_options" => [
+                    "label" => "Le mot de passe"
+                ],
+                "second_options" => [
+                    "label" => "Répetez le mot de passe"
+                ]
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'custom_option' => 'default'
+
         ]);
     }
 }
